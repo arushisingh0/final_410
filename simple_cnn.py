1bc8bbd01fe5902d1d350b19d3f197516071d680
arushisingh0 <118374149+arushisingh0@users.noreply.github.com>
2024-04-29 18:33:57 -0400

    Update simple_cnn.py

--- simple_cnn.py
+++ simple_cnn.py
@@ -7,8 +7,29 @@ from pyspark.ml.pipeline import Pipeline, PipelineModel
 from sparktorch import PysparkPipelineWrapper
 import torch
 import torch.nn as nn
-from sparktorch.examples.cnn_network import Net
+#from sparktorch.examples.cnn_network import Net
+import torch.nn.functional as F
 
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 5, 1)
+        self.conv2 = nn.Conv2d(16, 32, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.fc1 = nn.Linear(3872, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 1, 28, 28)
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        return x
 
 if __name__ == '__main__':
     spark = SparkSession.builder \
